667b21e77ebe95b93286bcae4201e305309df2fc
Dena <denaalt2004@gmail.com>
2024-08-06 20:14:48 +0300

    done

---
+++ jfunctions.js
@@ -0,0 +1,48 @@
+let firstName = "dena";
+
+function printName() {
+  console.log("dena");
+}
+
+printName();
+
+function printAge(yearOfBirth) {
+    let age = 2024 - yearOfBirth;
+  console.log(age);
+}
+printAge(2004);
+
+
+
+
+
+
+
+// task 4
+function printHello(firstName, language){
+if (language == `en`) {
+  console.log(`Hello, ${firstName}`);
+}else if(language == es){
+   ( language == `es` )
+   console.log(`Hello, ${firstName}`)
+}else if(language == fr){
+    console.log(`Hello, ${firstName}`)
+}else if (language == tr){
+    console.log(`Hello, ${firstName}`)
+}
+// task 5
+
+function printMax{
+if (a> b) {}
+    return a;
+}else {
+    return b;
+}
+}console.log(printMax (20, 15));
+
+
+
+
+
+
+
