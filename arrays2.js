183f35db7d09471ef9f44c543c6f00a99aa087b4
Dena <denaalt2004@gmail.com>
2024-08-08 16:35:31 +0300

    done

---
+++ arrays2.js
@@ -0,0 +1,23 @@
+// task1
+
+function isArrayLengthOdd(numbers) {
+
+    if (numbers.length %2 == 0)
+        {return false}
+}else{
+return true;
+}
+console.log (isArrayLengthOdd([1, 2, 3]));
+
+//task2
+
+function isArrayLengthEven(numbers){
+
+    
+}
+
+
+
+
+
+
